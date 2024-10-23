6229873d44594d0df5676fa439284e5caba83084
1ruslan9 <113448409+1ruslan9@users.noreply.github.com>
2024-10-24 04:28:00 +1000

    Update settings.py

--- homework/settings.py
+++ homework/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'hw2',
     'hw3',
     'hw4',
-    'debug_toolbar'
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -141,10 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATIC_ROOT = BASE_DIR/'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
